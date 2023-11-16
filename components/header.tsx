922407773b4828168fcb3a829c61eee013b6d5a4
Janella-codes <118861074+Janella-codes@users.noreply.github.com>
2023-11-15 21:56:52 -0800

    Add auth

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import { motion } from 'framer-motion';
-import React from 'react'
+import React, { useState } from 'react'
 import { links } from '@/lib/data'
 import Link from 'next/link';
+import clsx from 'clsx';
 
 
 export default function Header() {
+  const [activeSection, setActiveSection] = useState('Home');
+
   return (
   <header className='z-[999] relative'>
     <motion.div className='fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full'
@@ -28,9 +31,8 @@ export default function Header() {
           animate={{ y: 0, opacity: 1 }}
           >
            <Link
-              className='flex w-full items-center 
-              justify-center px-3 py-3
-              hover:text-gray-950 transition'
+              className= {clsx('flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition', { 'text-gray-950': activeSection === link.name })}
+              
               href={link.hash}
             >
             {link.name}
